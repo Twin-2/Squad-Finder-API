2f3d9d2d3a4b6e113531f61b2c5eefe734459fd2
Jaya DeHart <dehartj97@gmail.com>
2021-09-13 09:49:59 -0700

    modified error handlers

--- src/middleware/basicAuth.js
+++ src/middleware/basicAuth.js
@@ -1,22 +1,21 @@
 'use strict';
+var createError = require('http-errors');
 
 const base64 = require('base-64');
 const { users } = require('../schemas/index.js');
 
 module.exports = async (req, res, next) => {
-  if (!req.headers.authorization) { return _authError(); }
+  if (!req.headers.authorization) {
+    return next(createError('No such user', 403));
+  }
 
   let basic = req.headers.authorization.split(' ').pop();
   let [user, pass] = base64.decode(basic).split(':');
 
   try {
-    req.user = await users.model.authenticateBasic(user, pass)
+    req.user = await users.model.authenticateBasic(user, pass);
     next();
   } catch (e) {
-    _authError()
+    return next(createError('No such user', 403));
   }
-
-  function _authError() {
-    res.status(403).send('Invalid Login')
-  }
-} 
+};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const errorHandler = require('./error-handlers/errorHandler');
 //require error handlers
 //require routes
 
@@ -11,13 +12,15 @@ app.use(cors());
 //app.use routes
 //app.use errors
 
+app.use(errorHandler);
+
 function start(PORT) {
   app.listen(PORT, () => {
-    console.log(`Proof of life on ${PORT}`)
-  })
+    console.log(`Proof of life on ${PORT}`);
+  });
 }
 
 module.exports = {
   app,
-  start
-}
+  start,
+};
