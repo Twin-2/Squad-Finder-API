eac0e8c6ab771856f095b92afca7093a40160246
DevJessParker <jess.k.dempsey@gmail.com>
2021-09-13 14:37:51 -0700

    added blocked friend middleware check

--- src/middleware/acl.js
+++ src/middleware/acl.js
@@ -1,11 +1,9 @@
 'use strict';
 
-// const { users } = require('../schemas/index.js');
-
 module.exports = (capability) => {
   return (req, res, next) => {
     try {
-      if(req.users.capabilities.includes(capability)) {
+      if(req.user.capabilities.includes(capability)) {
         next()
       } else {
         next('Invalid Access')
